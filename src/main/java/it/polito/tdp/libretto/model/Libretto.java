890134c02dff87aae90042a605ad211a38577b6d
Lara Calzoni <Lara Calzoni@LAPTOP-5AHPQM23.home-life.hub>
2021-03-09 12:00:28 +0100

    Soluzione punto 2

--- src/main/java/it/polito/tdp/libretto/model/Libretto.java
+++ src/main/java/it/polito/tdp/libretto/model/Libretto.java
@@ -17,6 +17,49 @@ public class Libretto {
 		this.voti.add(v);
 		
 	}
+	/*
+	 
+	//modo 1
+	public void stampaVotiUguali( int punteggio) {
+		//il libretto stampa da solo i voti
+		System.out.println();
+		
+	}
+	
+	//modo 2
+	public String votiUguali(int punteggio) {
+		//calcola una stringa che contiene i voti, sarà poi il chiamante a stamparli
+		//ma cosa devo stampare?
+		// -> solo il nome?
+		// -> tutto il voto.toString()
+	}
+	*/
+	
+	//modo 3
+	//(ha un difetto: espone all'esterno il tipo di struttura dati che ho usato)
+	//restituisce solo i voti uguali al criterio
+	public List <Voto> listaVotiUguali ( int punteggio){
+	List <Voto> risultato = new ArrayList<>();
+	for(Voto v: this.voti) {
+		if(v.getVoto()==punteggio) {
+			risultato.add(v); //è lo stesso oggetto che appartiene a due liste diverse
+		}
+	}
+	return risultato;
+	}
+	
+	//modo 4 
+	//(il metodo migliore)
+	public Libretto votiUguali(int punteggio) {
+		Libretto risultato= new Libretto();
+		for(Voto v: this.voti) {
+			if(v.getVoto()==punteggio) {
+				risultato.add(v); 
+			}
+		}
+	return risultato;
+	}
+	
 	public String toString() {
 		String s = "";
 		for(Voto v: this.voti) {
